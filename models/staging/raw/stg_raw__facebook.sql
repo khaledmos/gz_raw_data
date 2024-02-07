562ec1e7b675b64cbb2cd2d2aa6bee4525fc13bb
Khaled Mostafa <khaledition@gmail.com>
2024-02-07 16:42:02 +0000

    paid_source change

--- models/staging/raw/stg_raw__facebook.sql
+++ models/staging/raw/stg_raw__facebook.sql
@@ -10,7 +10,7 @@ renamed as (
 
     select
         date_date,
-        paid_source,
+        paid_source AS the_paid_source_change,
         campaign_key,
         campgn_name AS campaign_name,
         cast(ads_cost as float64) as ads_cost,
